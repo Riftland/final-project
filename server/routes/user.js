2e546b86ce44d1d7e854a041a82b5a26b0a8867a
Riftland <fran.mscr@gmail.com>
2017-12-13 21:37:08 +0100

    combat system with types finished

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,42 +8,154 @@ const User = require('../models/User.model');
 /*
 Modelo para la construcción de peleas
 ---------------------------------------
-- Atacante: Defensores -
-- Normal: x1/2 Roca, x0 Ghost
-- Fight: x2 Normal-Rock-Ice, x1/2 Flying-Poison-Bug-Psych, x0 Ghost
-- Flying: x2 Fight-Bug-Grass, x1/2 Rock-Electric
-- Poison: x2 Bug-Grass, x1/2 Poison-Ground-Rock-Ghost
-- Ground: x2 Poison, Rock, Fire, Electric x1/2 Bug-Grass, x0 Flying
-- Rock: x2 Flying-Bug-Fire-Ice, x1/2 Fight-Ground
-- Bug: x2 Poison-Grass-Psych, x1/2 Fight-flying-Ghost-Fire
-- Ghost: x2 Ghost, x0 Normal-Psych
-- Fire: x2 Bug-Grass-Ice, x1/2 Rock-Fire-Water-Dragon
-- Water: x2 Ground-Rock-Fire, x1/2 Water-Grass-Dragon
-- Grass: 2x Ground-Rock-Water, x1/2 Flying-Poison-Bug-Fire-Grass-Dragon
-- Electric: 2x Flying-Water, x1/2 Grass-Electric-Dragon, x0 Ground
-- Psych: x2 Fight-Poison, x1/2 Psych
-- Ice: x2 Flying-Ground-Grass-Dragon, x1/2 Water
-- Dragon: x2 Dragon
 */
+const pokeTypes = [
+  // - Normal: x1/2 Roca, x0 Ghost
+  {
+    name: 'Normal',
+    strong: [],
+    weak: ['Rock'],
+    zero: ['Ghost']
+  },
+  // - Fight: x2 Normal-Rock-Ice, x1/2 Flying-Poison-Bug-Psych, x0 Ghost
+  {
+    name: 'Fight',
+    strong: ['Normal', 'Rock', 'Ice'],
+    weak: ['Flying', 'Poison', 'Bug', 'Psych'],
+    zero: ['Ghost']
+  },
+  // - Flying: x2 Fight-Bug-Grass, x1/2 Rock-Electric
+  {
+    name: 'Flying',
+    strong: ['Fight', 'Bug', 'Grass'],
+    weak: ['Rock', 'Electric'],
+    zero: []
+  },
+  // - Poison: x2 Bug-Grass, x1/2 Poison-Ground-Rock-Ghost
+  {
+    name: 'Poison',
+    strong: ['Bug', 'Grass'],
+    weak: ['Poison', 'Ground', 'Rock', 'Ghost'],
+    zero: []
+  },
+  // - Ground: x2 Poison, Rock, Fire, Electric x1/2 Bug-Grass, x0 Flying
+  {
+    name: 'Ground',
+    strong: ['Poison', 'Rock', 'Fire', 'Electric'],
+    weak: ['Bug', 'Grass'],
+    zero: ['Flying']
+  },
+  // - Rock: x2 Flying-Bug-Fire-Ice, x1/2 Fight-Ground
+  {
+    name: 'Rock',
+    strong: ['Flying', 'Bug', 'Ice'],
+    weak: ['Fight', 'Ground'],
+    zero: []
+  },
+  // - Bug: x2 Poison-Grass-Psych, x1/2 Fight-flying-Ghost-Fire
+  {
+    name: 'Bug',
+    strong: ['Poison', 'Grass', 'Psych'],
+    weak: ['Fight', 'Flying', 'Ghost', 'Fire'],
+    zero: []
+  },
+  // - Ghost: x2 Ghost, x0 Normal-Psych
+  {
+    name: 'Ghost',
+    strong: ['Ghost'],
+    weak: [],
+    zero: ['Normal', 'Psych']
+  },
+  // - Fire: x2 Bug-Grass-Ice, x1/2 Rock-Fire-Water-Dragon
+  {
+    name: 'Fire',
+    strong: ['Bug', 'Grass', 'Ice'],
+    weak: ['Rock', 'Fire', 'Water', 'Dragon'],
+    zero: []
+  },
+  // - Water: x2 Ground-Rock-Fire, x1/2 Water-Grass-Dragon
+  {
+    name: 'Water',
+    strong: ['Ground', 'Rock', 'Fire'],
+    weak: ['Water', 'Grass', 'Dragon'],
+    zero: []
+  },
+  // - Grass: 2x Ground-Rock-Water, x1/2 Flying-Poison-Bug-Fire-Grass-Dragon
+  {
+    name: 'Grass',
+    strong: ['Ground', 'Rock', 'Water'],
+    weak: ['Flying', 'Poison', 'Bug', 'Fire', 'Grass', 'Dragon'],
+    zero: []
+  },
+  // - Electric: 2x Flying-Water, x1/2 Grass-Electric-Dragon, x0 Ground
+  {
+    name: 'Electric',
+    strong: ['Flying', 'Water'],
+    weak: ['Grass', 'Electric', 'Dragon'],
+    zero: ['Ground']
+  },
+  // - Psych: x2 Fight-Poison, x1/2 Psych
+  {
+    name: 'Psych',
+    strong: ['Fight', 'Poison'],
+    weak: ['Psych'],
+    zero: []
+  },
+  // - Ice: x2 Flying-Ground-Grass-Dragon, x1/2 Water
+  {
+    name: 'Ice',
+    strong: ['Flying', 'Ground', 'Grass', 'Dragon'],
+    weak: ['Water'],
+    zero: []
+  },
+  // - Dragon: x2 Dragon
+  {
+    name: 'Dragon',
+    strong: ['Dragon'],
+    weak: [],
+    zero: []
+  }
+]
+
+function getMultiplier(attackType, typeRival){
+  let multiplier = 1;
+  for(i = 0; i < pokeTypes.length; i++){
+    if(pokeTypes[i].name === attackType){
+      if(pokeTypes[i].strong.includes(typeRival)){
+        multiplier = 2;
+        break;
+      } else if(pokeTypes[i].weak.includes(typeRival)){
+        multiplier = 0.5;
+        break;
+      } else if(pokeTypes[i].zero.includes(typeRival)){
+        multiplier = 0;
+        break;
+      } else {
+        multiplier = 1;
+        break;
+      }
+    }
+  }
+  console.log('ATACANTE: ' + attackType);
+  console.log('DEFENSOR: ' + typeRival);
+  return multiplier;
+}
 
 function getAttack(){
   let att;
   let n = ~~(Math.random() * 100);
   if(n >= 0 && n <= 50){
     att = 0;
-  } else{
-    if(n > 50 && n <= 70){
+  } else if(n > 50 && n <= 70){
       att = 1;
-    } else{
-      if(n > 70 && n <= 85){
+    } else if(n > 70 && n <= 85){
         att = 2;
       } else{
         att = 3;
       }
-    }
+    return att;
   }
-  return att;
-}
+
 
 function fight(user, rival) {
   let userTeam = user.team;
@@ -51,15 +163,13 @@ function fight(user, rival) {
   let userPokenames = user.pokeNames;
   let rivalPokenames = rival.pokeNames;
 
-  let totalDmg, att, turn;
+  let totalDmg, att, turn, multiplier, finalAttack;
 
   while(userTeam.length > 0 && rivalTeam.length > 0){
     if(userTeam[0].stats.speed >= rivalTeam[0].stats.speed){
       turn = userTeam[0];
-      break;
     } else {
       turn = rivalTeam[0];
-      break;
     }
 
     while(userTeam[0].stats.health > 0 && rivalTeam[0].stats.health > 0){
@@ -68,43 +178,49 @@ function fight(user, rival) {
 
       if(turn == userTeam[0]){
 
+        let defense = rivalTeam[0].type[0];
         att = userTeam[0].mvm[getAttack()];
-        if(rivalTeam[0].stats.defense >= userTeam[0].att.damage){
-          totalDmg = 0;
+        multiplier = getMultiplier(att.type, defense);
+        finalAttack = att.damage * multiplier
+        if(rivalTeam[0].stats.defense >= finalAttack){
+          totalDmg = 2;
         } else {
-          //Mejorar esto para la diferencia de tipos
-          totalDmg = userTeam[0].att.damage - rivalTeam[0].stats.defense;
+          totalDmg = finalAttack - rivalTeam[0].stats.defense;
         }
         rivalTeam[0].stats.health = rivalTeam[0].stats.health - totalDmg;
         //Aquí el push y console
-        msg = `${userPokenames[0]} hizo ${att.name} que causó ${totalDmg} de daño a ${rivalPokenames[0]}`
+        msg = `${userPokenames[0]} hizo ${att.name} que causó ${totalDmg} de daño a ${rivalPokenames[0]} - HP: ${rivalTeam[0].stats.health}`
         turn = rivalTeam[0]
 
       } else {
 
+        let defense = userTeam[0].type[0];
         att = rivalTeam[0].mvm[getAttack()];
-        if(userTeam[0].stats.defense >= rivalTeam[0].att.damage){
-          totalDmg = 0;
+        multiplier = getMultiplier(att.type, defense);
+        finalAttack = att.damage * multiplier;
+        if(userTeam[0].stats.defense >= finalAttack){
+          totalDmg = 2;
         } else {
-          //Mejorar esto para la diferencia de tipos
-          totalDmg = rivalTeam[0].att.damage - userTeam[0].stats.defense;
+          totalDmg = finalAttack - userTeam[0].stats.defense;
         }
         userTeam[0].stats.health = userTeam[0].stats.health - totalDmg;
         //Aquí el push y console
-        msg = `${rivalPokenames[0]} hizo ${att.name} que causó ${totalDmg} de daño a ${userPokenames[0]}`
+        msg = `${rivalPokenames[0]} hizo ${att.name} que causó ${totalDmg} de daño a ${userPokenames[0]} - HP: ${rivalTeam[0].stats.health}`
         turn = userTeam[0]
 
       }
 
-      console.log(``);
+      console.log(msg + ' | ' + finalAttack + ' | ' + totalDmg);
 
     }
 
     if(userTeam[0].stats.health <= 0){
+      console.log(`Ganador ${rivalPokenames[0]}`);
       userTeam.splice(0, 1);
       userPokenames.splice(0, 1);
     }
     if(rivalTeam[0].stats.health <= 0){
+      console.log(`Ganador ${userPokenames[0]}`);
       rivalTeam.splice(0, 1);
       userPokenames.splice(0, 1);
     }
